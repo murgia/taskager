70bf217354ec25f354951413b6d849101f90f6d1
Matthew Murgia <matthew.murgia@gmail.com>
2015-11-18 16:45:15 -0500

    complete feature to add tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,14 @@
-class MembersController < ApplicationController
+class TasksController < ApplicationController
   def new
     @task = Task.new
+    @memberships = Membership.find(params[:membership_id])
   end
 
   def create
+    @memberships = Membership.find(params[:membership_id])
+    @task = @memberships.tasks.create!(task_params)
+    @group = @memberships.group
+    redirect_to group_members_path(@group)
   end
 
   def edit
